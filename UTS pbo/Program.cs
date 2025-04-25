735b3ad0e547196a2108007ffbacd8a509b9b317
GWAH-berak <rafifauzi135@gmail.com>
2025-04-25 14:10:47 +0700

    Add project files.

---
+++ UTS pbo/Program.cs
@@ -0,0 +1,17 @@
+﻿//Sistem Manajemen Perpustakaan Mini 
+//Sebuah perpustakaan kecil ingin membuat sistem manajemen sederhana menggunakan 
+//aplikasi console. Dalam sistem ini, terdapat berbagai jenis buku seperti Buku Fiksi, Buku 
+//Non-Fiksi, dan Majalah. Setiap buku memiliki informasi dasar seperti judul, penulis, dan 
+//tahun terbit. 
+//Petugas ingin dapat melakukan hal-hal berikut: 
+//●  Menambahkan buku ke dalam daftar perpustakaan. 
+//●  Mengubah data buku 
+//●  Melihat informasi setiap buku secara umum. 
+//●  Menyimpan data buku dalam daftar menggunakan 
+//●  Meminjam buku (maksimal 3 buku). 
+//●  Mengembalikan buku. 
+//●  Melihat daftar buku yang sedang dipinjam. 
+//Syarat : Program menerapkan 4 pilar OOP yaitu inheritance, enkapsulasi, polymorphism,
+//abstraction dan interface.
+
+using System
